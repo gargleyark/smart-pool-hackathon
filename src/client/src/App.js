4f81239234a0a5bbc891e84762fc30514de7249b
Mike Steel <michael.steel@smartpension.co.uk>
2023-10-12 10:10:04 +0100

    really really really basic app with heroes chocolates

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,82 +1,38 @@
-import React, { useState, useEffect } from "react";
-import logo from "./react-logo.svg";
-import nodejsLogo from "./nodejs-logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import logo from './react-logo.svg'
+import nodejsLogo from './nodejs-logo.svg'
+import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
+  const [bookedStatus, setBookedStatus] = useState(null)
 
   useEffect(() => {
-    fetch("/api/users")
-      .then(res => res.json())
-      .then(json => setUsers(json.users));
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.users))
     // Specify how to clean up after this effect:
-    return () => {};
-  }, []); // empty 2nd arg - only runs once
+    console.log(users)
+
+    fetch('/api/pool-table-booked')
+      .then((res) => res.json())
+      .then((json) => setBookedStatus(json))
+  }, []) // empty 2nd arg - only runs once
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Node Template</h1>
-        <p>
-          <a
-            className="App-link"
-            href="https://github.com/mattvukas/react-node-template"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://github.com/mattvukas/react-node-template
-          </a>
-        </p>
-        <div className="logo-box">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={nodejsLogo} className="Node-logo" alt="nodejsLogo" />
-        </div>
-        <p>
-          Edit <code>client/src/App.js</code> and save to reload React app.
-        </p>
-        <p>
-          Edit <code>client/server/routes/api.js</code> and save to reload
-          Node.js app.
-        </p>
-        <br />
-        <p>
-          <code>GET /api/users</code>:{" "}
-          {users.length ? users.join(", ") : "loading..."}
-        </p>
-        <br />
-        <p>
-          Docs:{" "}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://nodejs.org/en/docs/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Node.js
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://expressjs.com/en/4x/api.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Express.js
-          </a>
-        </p>
+        {bookedStatus?.booked === false && (
+          <h1>
+            Sorry, table was already booked - try again in{' '}
+            {bookedStatus.timeLeft} minutes!
+          </h1>
+        )}
+        {bookedStatus?.booked === true && <h1>Pool table booked, enjoy!</h1>}
+        {!bookedStatus && <h1>Checking if pool table is booked...</h1>}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET users listing. */
-router.get("/users", function(req, res, next) {
-  console.log('get "users" route hit');
-  res.send({ users: ["joe", "bernie", "tulsi", "donald", "bill"] });
-});
+router.get('/users', function (req, res, next) {
+  console.log('get "users" route hit')
+  res.send({ users: ['joe', 'bernie', 'tulsi', 'donald', 'bill'] })
+})
 
-module.exports = router;
+router.get('/pool-table-booked', function (req, res, next) {
+  console.log('get "pool-table-booked" route hit')
+  res.send({ booked: false, timeLeft: 10 })
+})
+
+module.exports = router
