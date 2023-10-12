c80c1792ded2f3edb59be4611dcbe8c5b40a89c1
Mike Steel <michael.steel@smartpension.co.uk>
2023-10-12 15:22:59 +0100

    adds slack stuff

--- src/server/app.js
+++ src/server/app.js
@@ -1,50 +1,68 @@
-const { App, ExpressReceiver } = require('@slack/bolt')
-const express = require('express')
+// const { App, ExpressReceiver } = require('@slack/bolt')
+// const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+// const cookieParser = require('cookie-parser')
+// const logger = require('morgan')
 require('dotenv').config()
 
-const apiRouter = require('./routes/api')
+// const apiRouter = require('./routes/api')
 
-const app = express()
+// const app = express()
 
-// const boltApp = new App({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   token: process.env.SLACK_BOT_TOKEN,
-//   endpoints: '/',
+// // const boltApp = new App({
+// //   signingSecret: process.env.SLACK_SIGNING_SECRET,
+// //   token: process.env.SLACK_BOT_TOKEN,
+// //   endpoints: '/',
+// // })
+
+// // const boltReceiver = new ExpressReceiver({
+// //   signingSecret: process.env.SLACK_SIGNING_SECRET,
+// //   endpoints: '/',
+// // })
+// // const boltApp = new App({
+// //   token: process.env.SLACK_BOT_TOKEN,
+// //   receiver: boltReceiver,
+// // })
+
+// // boltApp.event('member_joined_channel', ({ event }) => console.log(event))
+// // boltApp.event('message', ({ event, say }) => say('hello'))
+
+// app.use(logger('dev'))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cookieParser())
+
+// // app.use('/slack/events', boltApp.receiver)
+
+// // mount our api router here
+// app.use('/api', apiRouter)
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../client/build')))
+
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   console.log('req.path', req.path)
+//   res.sendFile(path.join(__dirname + '../client/build/index.html'))
 // })
 
-// const boltReceiver = new ExpressReceiver({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   endpoints: '/',
-// })
-// const boltApp = new App({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   receiver: boltReceiver,
-// })
+const express = require('express')
+const { App, ExpressReceiver } = require('@slack/bolt')
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+})
+receiver.router.use(express.static(path.join(__dirname, '../client/build')))
+//receiver.router.use(express.json()) // if we wanted more than static pages
+const app = new App({ token: process.env.SLACK_BOT_TOKEN, receiver })
 
-// boltApp.event('member_joined_channel', ({ event }) => console.log(event))
-// boltApp.event('message', ({ event, say }) => say('hello'))
+// ;(async () => {
+//   await app.start(process.env.PORT || 3000)
+//   console.log('App is running; listening for events from Slack / the web')
+// })()
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-
-// app.use('/slack/events', boltApp.receiver)
-
-// mount our api router here
-app.use('/api', apiRouter)
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')))
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  console.log('req.path', req.path)
-  res.sendFile(path.join(__dirname + '../client/build/index.html'))
+app.message(/^ping$/i, async ({ context, say }) => {
+  await say('pong')
 })
 
 module.exports = app
