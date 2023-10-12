bc67d24150621360569a940826cc6bf42178abfc
james.kavanagh <james.kavanagh@smartpension.co.uk>
2023-10-12 16:21:41 +0100

    database changes again

--- src/server/bin/database.js
+++ src/server/bin/database.js
@@ -4,22 +4,18 @@ const pg = require('pg')
 
 const client = new pg.Client({
     host: process.env.DATABASE_URL,
-    port: process.env.PORT,
+    port: process.env.DATABSE_PORT,
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     ssl: true
 })
 
-async function connectDatabase(){
-  client.connect(function (err) {
-    if (!err) {
-      console.log('Database is connected!');
-    } else {
-      console.log('Error connecting database!');
-    }
-  });
-  return client
+async function query(sql){
+  await client.connect();
+  const result = await client.query(sql);
+  await client.end();
+  return result
 }
 
-module.exports = connectDatabase();
+module.exports = query;
--- src/server/routes/pool-table-booked/post.js
+++ src/server/routes/pool-table-booked/post.js
@@ -1,10 +1,10 @@
-var db = require('../../bin/database.js');
+var query = require('../../bin/database.js');
 const fifthteen = 900000;
 module.exports = postPoolTableBooked = async (req, res, next) => {
   console.log('post "pool-table-booked" route hit')
   try {
     
-    result = db.query(`INSERT INTO bookings(start_time, end_time) VALUES (to_timestamp(${Date.now()} / 1000.0), to_timestamp(${Date.now()} + ${fifthteen} / 1000.0));`)
+    result = query(`INSERT INTO bookings(start_time, end_time) VALUES (to_timestamp(${Date.now()} / 1000.0), to_timestamp((${Date.now()} + ${fifthteen}) / 1000.0));`)
 
     console.log(result)
 
@@ -12,6 +12,6 @@ module.exports = postPoolTableBooked = async (req, res, next) => {
     // return json.results || error
   } catch (e) {
     console.log(e)
-    return { error: result }
+    return { error: e }
   }
 }
