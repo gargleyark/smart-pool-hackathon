e37a97e7514aeaeef559b55396f9b8ff6fd70bb9
Mike Steel <michael.steel@smartpension.co.uk>
2023-10-12 15:51:29 +0100

    adds slack mocked endpoints

--- src/server/app.js
+++ src/server/app.js
@@ -4,48 +4,16 @@ const path = require('path')
 // const cookieParser = require('cookie-parser')
 // const logger = require('morgan')
 require('dotenv').config()
+const getPoolTableBooked = require('./routes/pool-table-booked/get')
+const postPoolTableBooked = require('./routes/pool-table-booked/post')
 
-// const apiRouter = require('./routes/api')
+const deleteScore = require('./routes/scoreboard/delete')
+const getScoreboard = require('./routes/scoreboard/get')
+const postScore = require('./routes/scoreboard/post')
 
-// const app = express()
+const putInOffice = require('./routes/in-office/put')
 
-// // const boltApp = new App({
-// //   signingSecret: process.env.SLACK_SIGNING_SECRET,
-// //   token: process.env.SLACK_BOT_TOKEN,
-// //   endpoints: '/',
-// // })
-
-// // const boltReceiver = new ExpressReceiver({
-// //   signingSecret: process.env.SLACK_SIGNING_SECRET,
-// //   endpoints: '/',
-// // })
-// // const boltApp = new App({
-// //   token: process.env.SLACK_BOT_TOKEN,
-// //   receiver: boltReceiver,
-// // })
-
-// // boltApp.event('member_joined_channel', ({ event }) => console.log(event))
-// // boltApp.event('message', ({ event, say }) => say('hello'))
-
-// app.use(logger('dev'))
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
-// app.use(cookieParser())
-
-// // app.use('/slack/events', boltApp.receiver)
-
-// // mount our api router here
-// app.use('/api', apiRouter)
-
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '../client/build')))
-
-// // The "catchall" handler: for any request that doesn't
-// // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   console.log('req.path', req.path)
-//   res.sendFile(path.join(__dirname + '../client/build/index.html'))
-// })
+const getSummary = require('./routes/summary/get')
 
 const express = require('express')
 const { App, ExpressReceiver } = require('@slack/bolt')
@@ -53,16 +21,30 @@ const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 })
 receiver.router.use(express.static(path.join(__dirname, '../client/build')))
-//receiver.router.use(express.json()) // if we wanted more than static pages
+receiver.router.get('/api/pool-table-booked', getPoolTableBooked)
+receiver.router.post('/api/pool-table-booked', postPoolTableBooked)
+receiver.router.get('/api/summary', getSummary)
+
 const app = new App({ token: process.env.SLACK_BOT_TOKEN, receiver })
 
-// ;(async () => {
-//   await app.start(process.env.PORT || 3000)
-//   console.log('App is running; listening for events from Slack / the web')
-// })()
+app.message(/^game.*/i, async ({ context, say }) => {
+  const newLeaderboard = await postScore(context)
+  await say(newLeaderboard)
+})
 
-app.message(/^ping$/i, async ({ context, say }) => {
-  await say('pong')
+app.message(/^delete last game$/i, async ({ context, say }) => {
+  const newLeaderboard = await deleteScore(context)
+  await say(newLeaderboard)
+})
+
+app.message(/^leaderboard$/i, async ({ context, say }) => {
+  const leaderboard = await getScoreboard(context)
+  await say(leaderboard)
+})
+
+app.message(/^in office$/i, async ({ context, say }) => {
+  const inOffice = await putInOffice(context)
+  await say(inOffice)
 })
 
 module.exports = app
--- src/server/routes/pool-table-booked/post.js
+++ src/server/routes/pool-table-booked/post.js
@@ -1,27 +1,5 @@
 const nodeFetch = require('node-fetch')
 module.exports = postPoolTableBooked = async (req, res, next) => {
-  console.log('post "pool-table-booked" route hit')
-  try {
-    const url = `https://api.sheetson.com/v2/sheets/pool`
-    const response = await nodeFetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.SHEETSON_API_KEY || ''}`,
-        'X-Sheetson-Spreadsheet-Id': process.env.SHEET_ID || '',
-      },
-      body: JSON.stringify({
-        inUse: 'FALSE',
-      }),
-    })
-    const json = await response.text()
-
-    console.log(json)
-
-    res.send({ json })
-    // return json.results || error
-  } catch (e) {
-    console.log(e)
-    return { error: JSON.stringify(e) }
-  }
+  res.send({ json: '' })
+  // return json.results || error
 }
