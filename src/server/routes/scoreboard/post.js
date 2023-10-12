131df375586133a7ad24032839e08abb331a1c66
Mike Steel <michael.steel@smartpension.co.uk>
2023-10-12 17:42:58 +0100

    set weighting

--- src/server/routes/scoreboard/post.js
+++ src/server/routes/scoreboard/post.js
@@ -1,3 +1,72 @@
-module.exports = postScore = ({ matches }) => {
-  return matches[0]
+require('dotenv').config()
+var query = require('../../bin/database.js')
+const trueskill = require('trueskill')
+
+module.exports = postScore = async ({ matches }) => {
+  const result = await query(`SELECT * from users ORDER BY weighting DESC;`)
+  const users = result.rows
+  const match = matches[0] //game james mike 5 10
+
+  const scores = match.match(/[^(game)][a-zA-Z]+/g)
+  const names = match.match(/\w^(\d)+/g)
+
+  const team1Score = scores[0]
+  const team2Score = scores[1]
+
+  const team1Ranking = team1Score > team2Score ? 1 : 0
+  const team2Ranking = team2Score > team1Score ? 1 : 0
+
+  const team1Names = names.length > 2 ? [names[0], names[1]] : [names[0]]
+  const team2Names = names.length > 2 ? [names[2], names[3]] : [names[1]]
+
+  const usersToUpdate = []
+  const warnings = []
+
+  team1Names.forEach((name) => {
+    console.log('updating', name.trim(), 'with', team1Score)
+    const user = users.find((user) => user.name === name.trim())
+
+    if (!user) {
+      warnings.push(`Could not find user ${name}, creating a new user`)
+      usersToUpdate.push({
+        name: name.trim(),
+        skill: [25.0, 25.0 / 3.0],
+        rank: team1Ranking,
+      })
+    } else {
+      const userWeighting = JSON.parse(user.weighting)
+      usersToUpdate.push({
+        name: user.name,
+        skill: userWeighting,
+        rank: team1Ranking,
+      })
+    }
+  })
+
+  team2Names.forEach((name) => {
+    console.log('updating', name.trim(), 'with', team2Score)
+    const user = users.find((user) => user.name === name.trim())
+    if (!user) {
+      warnings.push(`Could not find user ${name}, creating a new user`)
+      usersToUpdate.push({
+        name: name.trim(),
+        skill: [25.0, 25.0 / 3.0],
+        rank: team2Ranking,
+      })
+    } else {
+      const userWeighting = JSON.parse(user.weighting)
+      usersToUpdate.push({
+        name: user.name,
+        skill: userWeighting,
+        rank: team2Ranking,
+      })
+    }
+  })
+
+  trueskill.AdjustPlayers(usersToUpdate)
+
+  usersToUpdate.forEach(({ name, skill }) => {
+    const weighting = JSON.stringify(skill)
+    query(`UPDATE users SET weighting = '${weighting}' WHERE name = '${name}';`)
+  })
 }
